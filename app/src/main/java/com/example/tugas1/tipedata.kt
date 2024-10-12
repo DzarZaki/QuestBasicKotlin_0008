63dd13f96d6667723b6300e8f3ed3984b24eb115
DzarZaki <dzarzaki2@gmail.com>
2024-10-12 07:03:49 +0700

    Inisialisasi tipe data dasar Byte, Short, Int, Long, Float, Double, Char, dan Boolean.

---
+++ app/src/main/java/com/example/tugas1/tipedata.kt
@@ -0,0 +1,36 @@
+package com.example.tugas1
+
+fun main() {
+    //Byte
+    val byteValue: Byte = 10
+    println("Byte: $byteValue")
+
+    //Short
+    val shortValue: Short = 20
+    println("Short: $shortValue")
+
+    //Int
+    val intValue: Int = 30
+    println("Int: $intValue")
+
+    //Long
+    val longValue: Long = 40
+    println("long: $longValue")
+
+    //Float
+    val floatValue: Float = 50.6f
+    println("float: $floatValue")
+
+    //Double
+    val doubleValue: Double = 60.6
+    println("double: $doubleValue")
+
+    //Char
+    val charValue: Char = 'A'
+    println(charValue)
+
+    //Boolean
+    val booleanValue: Boolean = true
+    println("Boolean: $booleanValue")
+
+}
