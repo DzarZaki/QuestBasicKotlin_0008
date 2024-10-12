7bf727b8a471c44f44d77996027ad7cb8bcb1003
DzarZaki <dzarzaki2@gmail.com>
2024-10-12 07:48:46 +0700

    Menambahkan fungsi nullSafety untuk menunjukkan konsep null safety dalam Kotlin.

---
+++ app/src/main/java/com/example/tugas1/NullSafety.kt
@@ -0,0 +1,26 @@
+package com.example.tugas1
+
+fun nullSafety() {
+    // neverNull has String type
+    var neverNull: String = "This can't be null"
+
+    // Throws a compiler error
+    // neverNull = null
+
+    // nullable has nullable String type
+    var nullable: String? = "You can keep a null here"
+
+    // This is OK
+    nullable = null
+
+    // Check for null in conditions
+    if (neverNull == null) {
+        println("inferredNonNull is null")
+    } else {
+        println("inferredNonNull is not null")
+    }
+
+    // Safe call operator
+    println(neverNull.length)  // 18
+    println(nullable?.length)  // null
+}
