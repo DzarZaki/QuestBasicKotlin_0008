1f27f3e6a3f609e6f0918898ee26bc4fd37751c7
DzarZaki <dzarzaki2@gmail.com>
2024-10-12 07:19:33 +0700

    Menambahkan penggunaan fungsi equals dan toString pada objek DataClass.

--- app/src/main/java/com/example/tugas1/DataClass.kt
+++ app/src/main/java/com/example/tugas1/DataClass.kt
@@ -9,3 +9,14 @@ data class DataClass(
     val id: Int,
     var email: String
 )
+
+fun main() {
+    val data = DataClass(1, "indra@gmail.com")
+    println(data)
+
+    // Fungsi Equals
+    val data2 = DataClass(1, "makan@gmail.com")
+    //prinln(data == data2) // false
+
+
+}
